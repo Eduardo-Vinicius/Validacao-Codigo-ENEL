d4b4502ba152586d2cdfde7e3291526041efe3ef
eduardo-vinicius <eduardo.vinicius01@outlook.com>
2021-03-31 20:36:29 -0300

    new att

--- valida_enel.py
+++ valida_enel.py
@@ -1,6 +1,9 @@
 import re
 
-texto_enel = "00000348340483300100051Bloco 1A apto 44    ##SSP29Março     20211544230007A16894030000840001550"
+texto_enel = "00000348340483300100051Bloco 1A apto 44    ##SSP29Fevereiro 20211544230007A16894030000840001550"
+
+print(texto_enel[50:59])
+
 # print(len(texto_enel))
 
 # print(texto_enel[10:18])
@@ -52,7 +55,6 @@ def valida_cep(texto):
 
 
 def valida_numero(texto):
-
     numero = texto[18:23]
     if numero == "00000":
         print("*Erro no Número* - O número não pode conter apenas 0 Zero.")
@@ -60,6 +62,7 @@ def valida_numero(texto):
         print("*Erro no Número* - O número contém caracteres especiais!")
     else:
         valida_complemento(texto)
+
 
 def valida_complemento(texto):
     complemento = texto[23:42]
@@ -70,25 +73,49 @@ def valida_complemento(texto):
     else:
         valida_regiao(texto)
 
+
 def valida_regiao(texto):
-    regiao = texto[43:47]
-    if regiao[0,1] != "##":
+    regiao = texto[43:48]
+    print(regiao[0:2])
+    print(regiao[2:5])
+    if regiao[0:2] != "##":
         print("*Erro na Região* - A Região não possui '##' como complemento")
-    if regiao[2,4] != "SSP":
+    if regiao[2:5] != "SSP":
         print("*Erro na Região* - O Codigo da Região não é valido")
     else:
         valida_dia(texto)
 
-def valida_dia(texto):
-    dia = texto[48:49]
-    if is_int(dia) == True:
-       idia = converte_numero(dia)
+
+def valida_data(data):
+    dia, mes, ano = map(int, data.split('/'))
+    if mes < 1 or mes > 12 or ano <= 0:
+        print("*Erro na Data* - A data está incorreta!")
+    if mes in (1, 3, 5, 7, 8, 10, 12):
+        ultimo_dia = 31
+    elif mes == 2:
+        if (ano % 4 == 0) and (ano % 100 != 0 or ano % 400 == 0):
+            ultimo_dia = 29
+        else:
+            ultimo_dia = 28
     else:
+        ultimo_dia = 30
+
+    if dia < 1 or dia > ultimo_dia:
+        return False
+
+    return True
+
+
+def valida_dia(texto):
+    dia = texto[48:50]
+    idia = converte_numero(dia)
+    if is_int(idia) == False:
         print("*Erro no Dia* - O dia não é um número")
     if idia == 0 or idia > 31:
         print("*Erro no Dia* - Esse dia não é valido")
     else:
         valida_mes(texto)
+
 
 def valida_mes(texto):
     mes = texto[50:59]
@@ -96,6 +123,7 @@ def valida_mes(texto):
         print("*Erro no Mês* - Esse Mês não é valido")
     else:
         valida_ano(texto)
+
 
 def valida_ano(texto):
     ano = texto[60:63]
@@ -108,30 +136,34 @@ def valida_ano(texto):
     else:
         valida_hora(texto)
 
+
 def valida_hora(texto):
-    hora = converte_numero(texto[64:65])
+    hora = converte_numero(texto[64:66])
     if hora > 23 or hora == 0:
         print("*Erro na Hora* - A Hora não é valida")
     else:
         valida_minuto(texto)
 
+
 def valida_minuto(texto):
-    minuto = converte_numero(texto[66:67])
-    if minuto > 59:
+    minuto = converte_numero(texto[66:68])
+    if minuto > 59 or minuto < 0:
         print("*Erro no Minuto* - O Minuto não é valido")
     if contains_special(minuto) == True:
         print("*Erro no Minuto* - O Minuto não pode Possuir um caractere especial")
     else:
         valida_segundo(texto)
 
+
 def valida_segundo(texto):
-    segundo = converte_numero(texto[68:69])
-    if segundo > 59:
+    segundo = converte_numero(texto[68:70])
+    if segundo > 59 or minuto < 0:
         print("*Erro no Segundo* - O Segundo não é valido")
     if contains_special(segundo) == True:
         print("*Erro no Segundo* - O Segundo não pode Possuir um caractere especial")
     else:
         valida_medidor(texto)
+
 
 def valida_medidor(texto):
     medidor = texto[70:79]
@@ -142,12 +174,14 @@ def valida_medidor(texto):
     else:
         valida_aparelho(texto)
 
+
 def valida_aparelho(texto):
     aparelho = converte_numero(texto[80:81])
     if aparelho > 14 or aparelho <= 0:
         print("*Erro no Aparelho* - Esse Aparelho não é valido")
     else:
         valida_kw(texto)
+
 
 def valida_kw(texto):
     kw = texto[82:87]
@@ -160,13 +194,13 @@ def valida_kw(texto):
     else:
         valida_custo(texto)
 
+
 def valida_custo(texto):
     custo = converte_numero(texto[88:92] + "." + texto[92:94])
     if custo <= 0:
         print("*Erro no Custo* - O Custo não pode ser zero ou negativo")
     else:
         print("Tudo Okay")
-
 
 
 def converte_numero(x):
@@ -194,12 +228,14 @@ def contains_special(texto):
     if(string_check.search(texto) == None):
         return False
     return True
-def month_okay(n,mes):
+
+
+def month_okay(n, mes):
     if n == 1:
-        if mes[7,9] == "   ":
+        if mes[7, 9] == "   ":
             return True
     elif n == 2:
-        if mes[8,9] == " ":
+        if mes[8, 9] == " ":
             return True
     elif n == 3:
         if mes[5:9] == "     ":
@@ -214,7 +250,7 @@ def month_okay(n,mes):
         if mes[5:9] == "     ":
             return True
     elif n == 7:
-        if mes(5,9) == "     ":
+        if mes(5, 9) == "     ":
             return True
     elif n == 8:
         if mes[6:9] == "    ":
@@ -238,48 +274,42 @@ def month_okay(n,mes):
 def wicth_month(x):
     if x[0:6] == "Janeiro":
         return 1
-    
-    elif x[0:8]  == "Fevereiro":
+
+    elif x[0:8] == "Fevereiro":
         return 2
-    
+
     elif x[0:4] == "Março":
         return 3
-    
+
     elif x[0:4] == "Abril":
         return 4
-    
+
     elif x[0:3] == "Maio":
         return 5
-    
+
     elif x[0:4] == "Junho":
         return 6
-    
+
     elif x[0:4] == "Julho":
         return 7
-    
-    elif x[0,5] == "Agosto":
+
+    elif x[0:5] == "Agosto":
         return 8
-    
-    elif x[0,7] == "Setembro":
+
+    elif x[0:7] == "Setembro":
         return 9
-    
-    elif x[0,6] == "Outubro":
+
+    elif x[0:6] == "Outubro":
         return 10
-    
-    elif x[0,7] == "Novembro":
+
+    elif x[0:7] == "Novembro":
         return 11
-    
-    elif x[0,7] == "Dezembro":
+
+    elif x[0:7] == "Dezembro":
         return 12
 
     else:
         return 13
-
-
-
-
-
-
 
 
 valida_tamanho(texto_enel)
