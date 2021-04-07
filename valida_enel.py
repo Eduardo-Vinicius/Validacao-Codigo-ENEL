3e59da00878d1cb5d800e7dd61ccfd9e9c9c43c9
eduardo-vinicius <eduardo.vinicius01@outlook.com>
2021-04-06 23:47:31 -0300

    atualizando rep

--- valida_enel.py
+++ valida_enel.py
@@ -1,8 +1,26 @@
 import re
+import pprint
 
-lista_texto = ["00000348340483300100051Bloco 1A apto 44    ##SSP29Fevereiro 20211544230007A16894030000840001550",
+
+lista_texto = ["00000348340483300100051Bloco 1A apto 44    ##SSP29Dezembro  20211544230007A16894030000840001550",
                "000103483404833001sd00051Bloco 1A apto 44    ##SSP29Fevereiro 20211544230007A168940300020240001550",
-               "00000348340483300100051Blsoo 1A apto 44    ##MMG19Março    20211544230007A16894030000840042550"]
+               "00000348340483300100051Blsoo 1A apto 44    ##MMG19Março    20211544230007A16894030000840042550",
+               "00000348340483300100051Bloco 2B apto 44    ##SSP29Fevereiro 20211544230007A16894030000840001550",
+               "000003232340483300100051Bloco 1A apto 44    ##SSP22Fevereiro 20211544230007A16894030000840001550",
+               "00000348340483300100051Bloco 1A apto 44    ##SSP29Fevereiro 2022544230007A16894330000840001550",
+               "00000R540483300100051Bloco 1A apto 44    ##SSP32Fevereiro 20211544230007A16894030000840001550",
+               "000003483404832310100051Bloco 1A apto 44    ##SSP29Fevereiro 20211544230007A168940300034840001550",
+               "000003483404833033330051Bloco 1A apto 44    #SSP29F232 21154330007A16894030000840001550",
+               "00000348340483300100051Bloco 1A   ##SSP29Fevereiro 20211544230007A1689403000240001550",
+               "00000348340481100051Bloco 1A apto 44    ##SSP29Fevereiro 2022244230007A16894030000840001550",
+               "00O00348340483E00100051Bloco 1A a.to 44    ##S5P29Feveeir0 20211544230007A168940300008400015@0",
+               "00O00348340483E00100051Bloco 1A apto 999    ##S5P29Fev3eir0 20211544230007A1689403000084015@0",
+               "00000348340483300100051Bloco 1A apto 44    ##SSP29Junh0     00211544230007A16894030000840001550",
+               "00000348340483300100000Bloco 1A apto 00    ##SSP29Fevereiro 20211544230007A16894030000840001550",
+               "00000000000000000100051Bloco 1A apto 44    ##SSP29Fevereiro 20211544230007A16894030000840001550",
+               "99O00348340483E00100051Bloco 12 apt0 44    *#S5P29Feve&eir0 20211544230007A168940300008400019950",
+               "000003483404833001000Bloco 1A apto 44    ##SSP29Fevereiro 20211544230007A16894030000840001550",
+               "00000348340483300100051Bloco 1A apto 44    ##SSP29julia 20211544230007A16894030000840001550"]
 
 # texto_enel = "00000348340483300100051Bloco 1A apto 44    ##SSP29Fevereiro 20211544230007A16894030000840001550"
 
@@ -12,7 +30,7 @@ def show_error(line, error, field):
 
 
 x = show_error(1, "xablau", "febem")
-print(x)
+# print(x)
 
 # print(len(texto_enel))
 # print(texto_enel[10:18])
@@ -49,184 +67,191 @@ aparelhos = {
     "14": "Outros"
 }
 
-print(aparelhos["01"])
+# print(aparelhos["01"])
 
 
-def valida_tamanho(texto):
+def valida_tamanho(texto, linha):
     tamanho = len(texto)
     if tamanho > 95 or tamanho < 95:
-        print("tamanho incorreto, o código informado se encontra com erro!")
-    return valida_cliente(texto)
+        dic_erros[linha].append(
+            "Tamanho: 'tamanho incorreto, o código informado se encontra com erro!'")
+    return valida_cliente(texto, linha)
 
 
-def valida_cliente(texto):
+def valida_cliente(texto, linha):
     if texto[0:10] == "0000000000":
-        print("*Erro no Cliente* - Os números não podem ser 0! Erro no cliente!")
+        dic_erros[linha].append(
+            "Cliente: Os números não podem ser 0! Erro no cliente!")
     cliente = converte_numero(texto[0:10])
     if is_int(cliente) != True:
-        print("*Erro no Cliente* - Encontrado problema no código do cliente!")
-    else:
-        valida_cep(texto)
+        dic_erros[linha].append(
+            "Cliente: Encontrado problema no código do cliente!")
+    valida_cep(texto, linha)
 
 
-def valida_cep(texto):
+def valida_cep(texto, linha):
     cep = texto[10:18]
     if cep == "00000000":
-        print("*Erro no CEP* -  O Cep contém apenas o número 0 (Zero).")
+        dic_erros[linha].append("CEP: O Cep contém apenas o número 0 (Zero).")
     cep = converte_numero(cep)
     if is_int(cep) != True:
-        print("*Erro no CEP* - O Cep está incorreto, não contém apenas números!")
-    else:
-        valida_numero(texto)
+        dic_erros[linha].append(
+            "CEP: O Cep está incorreto, não contém apenas números!")
+    valida_numero(texto, linha)
 
 
-def valida_numero(texto):
+def valida_numero(texto, linha):
     numero = texto[18:23]
     if numero == "00000":
-        print("*Erro no Número* - O número não pode conter apenas 0 Zero.")
+        dic_erros[linha].append(
+            "Número - O número não pode conter apenas 0 Zero.")
     if contains_special(numero) == True:
-        print("*Erro no Número* - O número contém caracteres especiais!")
-    else:
-        valida_complemento(texto)
+        dic_erros[linha].append(
+            "Número - O número contém caracteres especiais!")
+    valida_complemento(texto, linha)
 
 
-def valida_complemento(texto):
+def valida_complemento(texto, linha):
     complemento = texto[23:42]
     if is_int(complemento) == True:
-        print("*Erro no Complemento* - O complemento só possui números")
+        dic_erros[linha].append(
+            "Complemento - O complemento só possui números")
     if contains_special(complemento) == True:
-        print("*Erro no Complemento* - O complemento não pode possuir caracter especial")
-    else:
-        valida_regiao(texto)
+        dic_erros[linha].append(
+            "Complemento - O complemento não pode possuir caracter especial")
+    valida_regiao(texto, linha)
 
 
-def valida_regiao(texto):
+def valida_regiao(texto, linha):
     regiao = texto[43:48]
     if regiao[0:2] != "##":
-        print("*Erro na Região* - A Região não possui '##' como complemento")
+        dic_erros[linha].append(
+            "Região - A Região não possui '##' como complemento")
     if regiao[2:5] != "SSP":
-        print("*Erro na Região* - O Codigo da Região não é valido")
-    else:
-        valida_dia(texto)
+        dic_erros[linha].append("Região - O Codigo da Região não é valido")
+    valida_dia(texto, linha)
 
 
-def valida_dia(texto):
+def valida_dia(texto, linha):
     dia = texto[48:50]
     idia = converte_numero(dia)
     if is_int(idia) == False:
-        print("*Erro no Dia* - O dia não é um número")
+        dic_erros[linha].append("Dia - O dia não é um número")
     if idia == 0 or idia > 31:
-        print("*Erro no Dia* - Esse dia não é valido")
-    else:
-        valida_mes(texto)
+        dic_erros[linha].append("Dia - Esse dia não é valido")
+    valida_mes(texto, linha)
 
 
-def valida_mes(texto):
+def valida_mes(texto, linha):
     mes = texto[50:60]
+
     for i in lista:
         if mes not in lista:
-            print("Não encontramos o mês na lista")
-    print("Tudo ok")
-    valida_ano(texto)
+            dic_erros[linha].append("Mês - Não encontramos o mês na lista")
+            break
+    valida_ano(texto, linha)
 
 
-def valida_ano(texto):
+def valida_ano(texto, linha):
     ano = texto[60:64]
 
     if ano == "0000":
-        print("*Erro no Ano* - O ano não pode ser somento '0'")
+        dic_erros[linha].append("Ano - O ano não pode ser somento '0'")
     if contains_special(ano) == True:
-        print("*Erro no Ano* - O ano não pode possuir caracteres especiais")
+        dic_erros[linha].append(
+            "Ano - O ano não pode possuir caracteres especiais")
     ano = converte_numero(ano)
     if is_int(ano) == False:
-        print("*Erro no Ano* - O ano deve possuir somente números")
+        dic_erros[linha].append("Ano - O ano deve possuir somente números")
 
-    else:
-        valida_hora(texto)
+    valida_hora(texto, linha)
 
 
-def valida_hora(texto):
+def valida_hora(texto, linha):
+
     hora = converte_numero(texto[64:66])
-    if hora > 23 or hora == 0:
-        print("*Erro na Hora* - A Hora não é valida")
-    else:
-        valida_minuto(texto)
+    if hora > 23 or hora < 0:
+        dic_erros[linha].append("Erro - A Hora não é valida")
+    valida_minuto(texto, linha)
 
 
-def valida_minuto(texto):
+def valida_minuto(texto, linha):
     minuto = texto[66:68]
     if contains_special(minuto) == True:
-        print("*Erro no Minuto* - O Minuto não pode Possuir um caractere especial")
+        dic_erros[linha].append(
+            "Minuto - O Minuto não pode Possuir um caractere especial")
     minuto = converte_numero(texto[66:68])
-    if minuto > 59 or minuto < 0:
-        print("*Erro no Minuto* - O Minuto não é valido")
 
-    else:
-        valida_segundo(texto)
+    if minuto > 59 or minuto < 0 or minuto == False:
+        dic_erros[linha].append("Minuto - O Minuto não é valido")
+
+    valida_segundo(texto, linha)
 
 
-def valida_segundo(texto):
+def valida_segundo(texto, linha):
     segundo = texto[68:70]
     if contains_special(segundo) == True:
-        print("*Erro no Segundo* - O Segundo não pode Possuir um caractere especial")
+        dic_erros[linha].append(
+            "Segundo - O Segundo não pode Possuir um caractere especial")
     segundo = converte_numero(texto[68:70])
     if segundo > 59 or segundo < 0:
-        print("*Erro no Segundo* - O Segundo não é valido")
+        dic_erros[linha].append("Segundo - O Segundo não é valido")
 
-    else:
-        valida_medidor(texto)
+    valida_medidor(texto, linha)
 
 
-def valida_medidor(texto):
+def valida_medidor(texto, linha):
     medidor = texto[70:79]
     if medidor.isspace() == True:
-        print("*Erro no Medidor* - O medidor não deve possuir espaços")
+        dic_erros[linha].append("Medidor - O medidor não deve possuir espaços")
     if medidor == "0000000000":
-        print("*Erro no Medidor* - O medidor não pode possuir somente zeros")
-    else:
-        valida_aparelho(texto)
+        dic_erros[linha].append(
+            "Medidor - O medidor não pode possuir somente zeros")
+    valida_aparelho(texto, linha)
 
 
-def valida_aparelho(texto):
+def valida_aparelho(texto, linha):
     aparelho = converte_numero(texto[80:82])
-    print(aparelho)
     if aparelho > 14 or aparelho <= 0:
-        print("*Erro no Aparelho* - Esse Aparelho não é valido")
-    else:
-        valida_kw(texto)
+        dic_erros[linha].append("Aparelho - Esse Aparelho não é valido")
+    valida_kw(texto, linha)
 
 
-def valida_kw(texto):
+def valida_kw(texto, linha):
     kw = texto[82:87]
     if contains_special(kw):
-        print("*Erro no KW* - O kw não deve conter caracteres especiais")
+        dic_erros[linha].append(
+            "KW - O kw não deve conter caracteres especiais")
 
     kw = converte_numero(texto[82:87])
     if is_int(kw) == False:
-        print("*Erro no KW* - O kw deve conter somente numeros")
+        dic_erros[linha].append("KW - O kw deve conter somente numeros")
     if kw == "000000":
-        print("*Erro no KW* - O kw não pode possuir somente zeros")
+        dic_erros[linha].append("KW - O kw não pode possuir somente zeros")
 
-    else:
-        valida_custo(texto)
+    valida_custo(texto, linha)
 
 
-def valida_custo(texto):
+def valida_custo(texto, linha):
     custo = texto[88:95]
-
     x = custo[0:5] + "." + custo[5:7]
-    x = float(x)
-    if x <= 0:
-        print("*Erro no Custo* - O Custo não pode ser zero ou negativo")
+    if custo == "":
+        dic_erros[linha].append("Custo - Não possui caracteres")
     else:
-        print("Tudo Okay")
+        if contains_special(x) == True:
+            dic_erros[linha].append("Custo - Contém caracteres especiais")
+        else:
+            x = float(x)
+            if x <= 0:
+                dic_erros[linha].append(
+                    "Custo - O Custo não pode ser zero ou negativo")
 
 
 def converte_numero(x):
     try:
         return int(x)
     except ValueError:
-        print("O texto não contém apenas números")
+        return False
 
 
 def converte_decimal(x):
@@ -234,6 +259,7 @@ def converte_decimal(x):
         return float(x)
     except ValueError:
         print("O texto não contém apenas números")
+        return False
 
 
 def is_float(x):
@@ -256,8 +282,18 @@ def contains_special(texto):
     return True
 
 
+dic_erros = {}
+
 if __name__ == "__main__":
     tamanho = len(lista_texto)
-
+    contador = 1
     for i in lista_texto:
-        valida_tamanho(i)
+        dic_erros[contador] = [""]
+
+        print(f"Linha {contador}")
+        valida_tamanho(i, contador)
+        print("__________________________________________")
+        print(" ")
+
+        contador += 1
+pprint.pprint(dic_erros)
