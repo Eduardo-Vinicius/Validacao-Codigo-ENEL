e67bc8191d8e7b76d8ade1e747a887be6fba6a16
eduardo-vinicius <eduardo.vinicius01@outlook.com>
2021-04-08 21:58:26 -0300

    'att'

--- api.py
+++ api.py
@@ -4,10 +4,18 @@ import pprint
 
 
 def buscar_dados():
-    #request = requests.get("https://viacep.com.br/ws/04855450/json")
+    # request = requests.get("https://viacep.com.br/ws/04855450/json")
     request = requests.get(
         "https://devmonk-energymeter.herokuapp.com/EnergyMeter")
-    pprint.pprint(json.loads(request.content))
+    x = json.loads(request.content)
+    return create_code(x)
+
+
+def create_code(code):
+    for i in code:
+        pprint.pprint(i)
+        for j in i["energyData"]:
+            pprint.pprint(j)
 
 
 if __name__ == '__main__':
