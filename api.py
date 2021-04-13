8c858abde7d8d63792c3cbdc700cef43788014f3
eduardo-vinicius <eduardo.vinicius01@outlook.com>
2021-04-12 21:17:54 -0300

    'febemfudido'

--- api.py
+++ api.py
@@ -2,6 +2,8 @@ import requests
 import json
 import pprint
 
+lista_arquivo = []
+
 
 def buscar_dados():
     # request = requests.get("https://viacep.com.br/ws/04855450/json")
@@ -12,13 +14,26 @@ def buscar_dados():
 
 
 def client_code(code):
-    x = str(code[0]["clientCode"])
+    # pprint.pprint(code)
+    x = int(code[0]["clientCode"])
+    y = str(x)
     tamanho = len(str(x))
 
     if tamanho < 10:
-        dif = 10 - tamanho
-        x = x.zfill(dif)
-    return x
+        x = y.zfill(10)
+    return cep_code(code, x)
+
+
+def cep_code(code, linha):
+    x = code[0]["zipCode"]
+    cep = x.replace("-", "")
+    y = linha + cep
+    print(y)
+    return complement_code(code, y)
+
+
+def complement_code(code, linha):
+    pass
 
 
 if __name__ == '__main__':
